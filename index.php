b0a35472b598d718aefdc8c33b6dd6cf6e19b428
Matei Oprea <eu@opreamatei.ro>
2014-01-03 18:21:30 +0200

    Edited some things

--- index.php
+++ index.php
@@ -1,12 +1,34 @@
 <html>
+<head>
+<link rel="stylesheet" href="http://yui.yahooapis.com/pure/0.3.0/pure-min.css">
+</head>
 <body>
-<form method="post" action="">
-Language:<input type="text" size="12" name="lang"><br />
-<input type="submit" name"submit" value="submit">
+<center>
+<div class="pure-menu pure-menu-open pure-menu-horizontal">
+	<ul>
+		<li><a href="#">Home</a></li>
+		<li><a href="#">Contact</a><li>
+	</ul>
+</div></center>
+<form class="pure-form pure-form-aligned" method="post" action="">
+<fieldset>
+	<div class="pure-control-group">
+		<label for="language"><h2 class="content-subhead"></h2></label>
+		<input type="text" size="18" name="lang" placeholder="Programming Language" required>
+	</div>
+	<div class="pure-control-group">
+		<label for="results"></label>
+		<input type="text" size="12" name="resu" placeholder="Results? i.e. 5"><br />
+	</div>
+	<div class="pure-controls">
+		<button type="submit" class="pure-button pure-button-primary">Submit</button>
+	</div>
+</fieldset>
 </form>
 <?php
 function data_p(){
 	$lang= $_POST['lang'];
+	$res= $_POST['resu'];
 $ch = curl_init();
 curl_setopt($ch, CURLOPT_URL, 'https://api.github.com/legacy/repos/search/language:' . $lang);
 curl_setopt($ch, CURLOPT_RETURNTRANSFER,1);
@@ -15,10 +37,12 @@ curl_setopt($ch, CURLOPT_USERAGENT, 'mateioprea');
 $resp = curl_exec($ch);
 curl_close($ch);
 $jarr = json_decode($resp,1);
-for ($i=0; $i<=10; $i++){
-	echo $jarr['repositories'][$i]['url'] . "\n";
-	echo $jarr['repositories'][$i]['username'] . "\n";
-	echo $jarr['repositories'][$i]['description'] . "\n";
+for ($i=0; $i<$res; $i++){
+	echo "<blockquote>";
+	echo $jarr['repositories'][$i]['url'] . "<br />";
+	echo $jarr['repositories'][$i]['username'] . "<br />";
+	echo $jarr['repositories'][$i]['description'] . "<br />";
+	echo "</blockquote>";
 }
 }
 if (isset($_POST['lang'])){
