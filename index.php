33198fad8a749b6b8a2b8f39d1f37af15a936e50
Matei Oprea <eu@opreamatei.ro>
2014-01-03 18:33:41 +0200

    modified title with description

--- index.php
+++ index.php
@@ -39,10 +39,9 @@ curl_close($ch);
 $jarr = json_decode($resp,1);
 for ($i=0; $i<$res; $i++){
 	echo "<blockquote>";
-	echo "<h2> Project title:" . $jarr['repositories'][$i]['title'] . "<br />" . "</h2>";
+	echo "<h2> Project title:" . $jarr['repositories'][$i]['description'] . "<br />" . "</h2>";
 	echo $jarr['repositories'][$i]['url'] . "<br />";
 	echo $jarr['repositories'][$i]['username'] . "<br />";
-	echo $jarr['repositories'][$i]['description'] . "<br />";
 	echo "</blockquote> <hr width="70%" size="1px">  ";
 }
 }
