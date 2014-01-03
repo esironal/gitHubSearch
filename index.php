a2e37a33828b02326a66daaa373f9257bc77376a
Matei Oprea <eu@opreamatei.ro>
2014-01-03 16:22:30 +0200

    POST request now works

--- index.php
+++ index.php
@@ -1,9 +1,12 @@
 <html>
 <body>
-<input type="submit" name"btn_submit" value="click">
+<form method="post" action="">
+Language:<input type="text" size="12" name="lang"><br />
+<input type="submit" name"submit" value="submit">
+</form>
 <?php
-/* Function that gets the content from github for $language */
-function get_content ($lang){
+function data_p(){
+	$lang= $_POST['lang'];
 $ch = curl_init();
 curl_setopt($ch, CURLOPT_URL, 'https://api.github.com/legacy/repos/search/language:' . $lang);
 curl_setopt($ch, CURLOPT_RETURNTRANSFER,1);
@@ -11,7 +14,15 @@ curl_setopt($ch, CURLOPT_CONNECTTIMEOUT,1);
 curl_setopt($ch, CURLOPT_USERAGENT, 'mateioprea');
 $resp = curl_exec($ch);
 curl_close($ch);
-return $resp;
+$jarr = json_decode($resp,1);
+for ($i=0; $i<=10; $i++){
+	echo $jarr['repositories'][$i]['url'] . "\n";
+	echo $jarr['repositories'][$i]['username'] . "\n";
+	echo $jarr['repositories'][$i]['description'] . "\n";
+}
+}
+if (isset($_POST['lang'])){
+	data_p();
 }
 ?>
 </body>
